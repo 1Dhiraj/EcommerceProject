bb5380f356723944917c095b78d23f57cf477475
1Dhiraj <dhirajeng53@gmail.com>
2025-04-22 18:28:54 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+                An e-commerce Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quia, voluptas fugit a assumenda deserunt voluptatem ullam modi. 
+                Sunt enim et cupiditate aliquid, doloremque ipsum modi quisquam eum?
+                Temporibus tempora laborum modi similique placeat, harum consectetur. 
+                Distinctio, ab necessitatibus? Dolorem, exercitationem! Facilis ea et,
+                laborum repellendus quas impedit. Fugit, ad! Quis totam aut harum voluptate officia excepturi distinctio possimus sit libero repellat. Odit illum voluptatem repellendus officia, possimus natus, consequuntur fugit modi omnis animi repellat alias totam eum mollitia hic amet, magni voluptas cumque numquam. Aperiam ad repellendus, aliquid ullam repudiandae voluptatem hic similique possimus, totam cum, quam sint tenetur ducimus.
+
+            </p>
+            <p>
+                E-commerce websites typically display Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus consectetur voluptatum quisquam repellendus minus animi amet nobis eos. Doloribus maxime inventore quam tempore dolorem veritatis cum consequuntur, cupiditate nobis! Tenetur iste inventore assumenda cum sed repellat, delectus velit magnam totam. Itaque eum porro distinctio! Dolore qui ullam rerum suscipit! Accusamus nostrum adipisci soluta tenetur dolorum commodi temporibus sapiente sint ad consectetur ducimus, nisi ipsum sit maiores vel ullam incidunt. Magnam nulla voluptas exercitationem, molestiae explicabo quis aliquid cupiditate expedita voluptates, iure beatae distinctio? Distinctio repellendus repellat magnam a ab animi iste pariatur exercitationem odio veniam explicabo, asperiores tempore corrupti error.
+                
+
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
